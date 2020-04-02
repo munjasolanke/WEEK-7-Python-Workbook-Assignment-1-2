374839c7494e6d167b572bd55de90bcf51181cc6
Munja Dnyanoba Solanke <54603212+munjasolanke@users.noreply.github.com>
2020-04-02 12:58:55 -0400

    WEEK 7-Python Workbook Assignmet-1-2

---
+++ Assignment1_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Week-7-Homework
+#Assignment-1-2
+
+import pandas as pd
+Location = "C:/Users/munja/Downloads/BNK01.xls"
+df = pd.read_excel(Location)
+
+
+# In[2]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
